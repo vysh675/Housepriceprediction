5d502a0f424a10b7be200188e5a61d346173968e
vysh675 <83393974+vysh675@users.noreply.github.com>
2021-05-24 00:50:05 +0530

    Add files via upload

---
+++ housepriceprediction.py
@@ -0,0 +1,36 @@
+
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+import pandas as pd
+
+dataset = pd.read_csv('landpriceprediction.csv')
+
+
+land=dataset.drop('price',axis='columns')
+
+land
+
+price=dataset.price
+
+
+from sklearn import linear_model
+
+model=linear_model.LinearRegression()
+
+model.fit(land,price)
+
+
+#predicting the land price
+
+predictions=model.predict(land)
+plt.scatter(price,predictions)
+
+
+#visualizing the data
+
+import seaborn as sns
+sns.distplot((land),bins=50)
+
